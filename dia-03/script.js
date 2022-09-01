c5dd8392d401ddb37bce6d304f5be3471985c378
Jefferson-HSL <santoslima963@gmail.com>
2022-09-01 16:05:37 -0300

    last commit

---
+++ dia-03/script.js
@@ -0,0 +1,6 @@
+function not (){
+    addEventListener('click', function(){
+        window.location.assign("https://www.youtube.com/watch?v=9bZkp7q19f0#t=00m09s");
+        window.alert('Eu te avisei!!!!!');
+    })
+}
